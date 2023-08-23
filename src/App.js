8a6e8de40938f9959eb5d6c2cd16afe91b2afb8b
Faisal <faisalvin15@gmail.com>
2023-08-23 21:52:17 +0700

    add full code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Accordion />
+    </div>
+  );
+}
+
+function Accordion() {
+  const [curOpen, setCurOpen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {faqs.map((faq, i) => (
+        <Item
+          title={faq.title}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          index={i}
+          key={faq.title}
         >
-          Learn React
-        </a>
-      </header>
+          {faq.text}
+        </Item>
+      ))}
+      <Item
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        index={23}
+        key={23}
+        title={"What's your life"}
+      >
+        some item
+      </Item>
+    </div>
+  );
+}
+
+function Item({ title, index, curOpen, onOpen, children }) {
+  // const [isOpen, setIsOpen] = useState(false);
+  const isOpen = index === curOpen;
+
+  function handleIsOpen() {
+    onOpen(isOpen ? null : index);
+  }
+
+  return (
+    <div className={isOpen ? "item open" : "item"} onClick={handleIsOpen}>
+      <h2 className="number">{index < 10 ? `0${index + 1}` : index + 1}</h2>
+      <h2 className={isOpen ? "title text" : "title"}>{title}</h2>
+      <span className="icon">{isOpen ? "-" : "+"}</span>
+      {isOpen && (
+        <div className="content-box">
+          <p>{children}</p>
+        </div>
+      )}
     </div>
   );
 }
